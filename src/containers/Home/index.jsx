1b3c5fefc8f8ce84592a6acc57842847f096eab1
carloshenrique2026 <fariascarlos2346@gmail.com>
2026-03-31 10:59:11 -0300

    botao criado com sucesso

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,18 @@
 import { CategoriesCarousel } from '../../components/CategoriesCarousel';
 import { OffersCarousel } from '../../components/OffersCarousel';
+import { useNavigate } from 'react-router-dom';
 import { Banner, Container } from './styles';
 
 export function Home() {
+     const navigate = useNavigate();
     return (
         <main>
             <Banner>
+                <button onClick={() => navigate(-1)}>
+                    ⬅ Voltar
+                </button>
+
+
                 <h1>Bem-Vindos</h1>
             </Banner>
             <Container>
