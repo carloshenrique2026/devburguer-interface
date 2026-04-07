29bbabfca0c66f2b9776507c183391c285c806ec
carloshenrique2026 <fariascarlos2346@gmail.com>
2026-04-06 23:15:40 -0300

    Prosseguindo com Carrinho e sua  Logica

--- src/containers/Cart/styles.js
+++ src/containers/Cart/styles.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
+import Texture from '../../assets/texture.svg';
 
-export const Container = styled.div``
+export const Container = styled.div`
+    width: 100%;
+    background-color: #ffffff;
+    min-height: 100%;
+`;
 
-;
+export const Banner = styled.div`
+    background: url('${Texture}');
 
-export const Banner = styled.div``
+`;
     
-;
+
 
 
 export const Title = styled.div``
