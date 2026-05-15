f39ef49453388c3c210c41fffd1806d933f25890
carloshenrique2026 <fariascarlos2346@gmail.com>
2026-05-15 20:55:26 -0300

    Ajuste menu lateral

--- src/components/SideNavAdmin/index.jsx
+++ src/components/SideNavAdmin/index.jsx
@@ -1,18 +1,25 @@
+import { SignOut } from "@phosphor-icons/react";
+
 import { navLinks } from "./navLinks";
 import Logo1 from '../../assets/logo1.svg';
-import { SignOut } from "@phosphor-icons/react";
-import {  Container, NavLink, NavLinkContainer, Footer } from './styles.js';
+import { Container, NavLink, NavLinkContainer, Footer } from './styles.js';
 import { useUser } from '../../hooks/UserContext';
+import { useResolvedPath } from "react-router-dom";
 
 export function SideNavAdmin() {
     const { logout } = useUser();
+    const { pathname } = useResolvedPath();
 
     return (
         <Container>
             <img src={Logo1} alt="Hamburger Logo DevBurger" />
             <NavLinkContainer>
-                {navLinks.map((link) =>  (
-                    <NavLink  key={link.id} to={link.path}>
+                {navLinks.map((link) => (
+                    <NavLink
+                        key={link.id}
+                        to={link.path}
+                        $isActive={pathname === link.path}
+                    >
                         {link.icon}
                         <span>{link.label}</span>
                     </NavLink>
